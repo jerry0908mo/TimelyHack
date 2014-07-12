a5dfbd76c5f50f933055ec9932a59ec2a03713ae
jerry0908mo <jerry@duotin.com>
2014-07-12 14:04:45 +0800

    add widget package

---
+++ src/com/duotin/timelyhack/widget/package-info.java
@@ -0,0 +1,18 @@
+/*
+ * package-info.java
+ * @include classes:package-info;interfaces:package-info
+ * @version 1.0.0
+ * @data 2014年7月12日
+ * @copyright Copyright (C) 2012-2020 DuoTin Network Technology Co.,LTD 
+ */
+/**
+ * @name      package-info
+ * @desc      
+ * @package   com.duotin.timelyhack.widget
+ * @author    jerry@duotin.com
+ * @copyright Copyright (C) 2012-2020 DuoTin Network Technology Co.,LTD
+ * @version   1.0.0
+ * @history
+ * jerrysher 2014年7月12日 下午2:04:18 
+ */
+package com.duotin.timelyhack.widget;
